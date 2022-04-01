e3f408503ed5a70fb13d801e9c4b0715136ca5f1
David Vincze <github@kamm.hu>
2022-04-01 13:31:25 +0900

    main WIL initial

--- WIL.py
+++ WIL.py
@@ -13,17 +13,53 @@ from WIL_LocDataROS        import WILLocDataROS
 from WIL_LocDataRemoteUDP  import WILLocDataRemoteUDP
 
 
-class WILLocData:
+class WIL:
 
     def __init__(self, locdatasource, playareapixels):
+
+        self.trackers = {};
+        self.trackers_by_serial = {};
+        self.trackers_by_name = self.trackers;
+        self.locdatasource = locdatasource;
+        self.verbose = False;
+
         if   locdatasource == wil_config.LOCDATA_SOURCE_LIBSURVIVE:
-            self.dataiface = WILLocDataLibSurvive(playareapixels);
+            self.locdata = WILLocDataLibSurvive(playareapixels);
         elif locdatasource == wil_config.LOCDATA_SOURCE_STEAMVR:
-            self.dataiface = WILLocDataSteamVR(playareapixels);
+            self.locdata = WILLocDataSteamVR(playareapixels);
         elif locdatasource == wil_config.LOCDATA_SOURCE_ROS:
-            self.dataiface = WILLocDataROS(playareapixels);
+            self.locdata = WILLocDataROS(playareapixels);
         elif locdatasource == wil_config.LOCDATA_SOURCE_REMOTEUDP:
-            self.dataiface = WILLocDataRemoteUDP(playareapixels);
+            self.locdata = WILLocDataRemoteUDP(playareapixels);
         else:
-            print("Unknown locdata_source was specified. Exiting.");
+            print("Unknown LocData_source was specified. Exiting.");
             sys.exit(1);  # should be an exception. maybe later...
+
+    def set_verbose(self, verbose):
+        self.verbose = verbose;
+        self.locdata.verbose = verbose;
+
+    def add_tracker(self, trackername, trackerserial):
+        self.trackers_by_serial[trackerserial] = self.locdata.add_tracker_by_serial(trackerserial);
+        self.trackers[trackername] = self.trackers_by_serial[trackerserial];
+
+    def get_tracker_names(self):
+        return self.trackers.keys();
+
+    def get_tracker_serials(self):
+        return self.trackers_by_serial.keys();
+
+    def calibrate(self, xoffset, yoffset, zoffset, rotoffset_world, rotoffset_obj, pixelratio, swapx, swapy, reverse_rotdir):
+        self.locdata.calibrate(xoffset, yoffset, zoffset, rotoffset_world, rotoffset_obj, pixelratio, swapx, swapy, reverse_rotdir);
+
+    def calibrate_from_file(self, calibdata_filename):
+        return self.locdata.calibrate_from_file(calibdata_filename);
+
+    def get_calibration_data(self):
+        return self.locdata.get_calibration_data();
+
+    def update(self):
+        return self.locdata.update();
+
+    def all_poses_valid(self):
+        return self.locdata.all_poses_valid();
