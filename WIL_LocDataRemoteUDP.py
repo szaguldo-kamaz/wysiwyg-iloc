f50d4f27e4a60f1c6b3e7258b0d4d93af0de6136
David Vincze <github@kamm.hu>
2023-11-17 01:33:50 +0900

    struct.pack/unpack combined into one, plus cleanup

--- WIL_LocDataRemoteUDP.py
+++ WIL_LocDataRemoteUDP.py
@@ -14,50 +14,44 @@ import socket, select, struct, sys
 class WILLocDataRemoteUDP(WILLocDataBase):
 
     def __init__(self, roomsize):
-        WILLocDataBase.__init__(self, roomsize);
-        bindip = "0.0.0.0";
-        portno_trackerinfo = 60003;
-        self.sock_trackerinfo = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-        self.sock_trackerinfo.bind((bindip, portno_trackerinfo));
-        self.last_packetno = -1;
+        WILLocDataBase.__init__(self, roomsize)
+        bindip = "0.0.0.0"
+        portno_trackerinfo = 60003
+        self.sock_trackerinfo = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock_trackerinfo.bind((bindip, portno_trackerinfo))
+        self.last_packetno = -1
 
 
     def __del__(self):
-        self.sock_trackerinfo.close();
+        self.sock_trackerinfo.close()
 
 
     def update_poses_from_src(self):
 
-        sockreadable, _, _ = select.select([self.sock_trackerinfo], [], [], 0);
+        sockreadable, _, _ = select.select([self.sock_trackerinfo], [], [], 0)
         while len(sockreadable) > 0:
             if self.sock_trackerinfo in sockreadable:
-                data, addr = self.sock_trackerinfo.recvfrom(72);
+                data, addr = self.sock_trackerinfo.recvfrom(72)
                 if len(data) < 72:
-                    sys.stderr.writelines("WILLocDataRemoteUDP.update_poses_from_src: Dropped too small (%d bytes data) packet! Contents: -%s-\n"%(len(data), data));
+                    sys.stderr.writelines("WILLocDataRemoteUDP.update_poses_from_src: Dropped too small (%d bytes data) packet! Contents: -%s-\n"%(len(data), data))
                     return
 
-                packetno_recv = (data[0] << 8) + data[1];
+                packetno_recv = (data[0] << 8) + data[1]
                 if packetno_recv > self.last_packetno:
                     if packetno_recv == 65535:
-                        self.last_packetno = 0;
+                        self.last_packetno = 0
                     else:
-                        self.last_packetno = packetno_recv;
-                    trackerserial = data[2:14];
-                    trackerserial = trackerserial.decode('ascii');
-                    trackerpose   = [ struct.unpack("d", data[14:22])[0],
-                                      struct.unpack("d", data[22:30])[0],
-                                      struct.unpack("d", data[30:38])[0],
-                                      struct.unpack("d", data[38:46])[0],
-                                      struct.unpack("d", data[46:54])[0],
-                                      struct.unpack("d", data[54:62])[0],
-                                      struct.unpack("d", data[62:70])[0] ];
-                    trackerbutton = data[71];
+                        self.last_packetno = packetno_recv
+                    trackerserial = data[2:14]
+                    trackerserial = trackerserial.decode('ascii')
+                    trackerpose   = list(struct.unpack("ddddddd", data[14:70]))
+                    trackerbutton = data[71]
 
-                    self.tracked_objects[trackerserial].pose = trackerpose;
+                    self.tracked_objects[trackerserial].pose = trackerpose
                     if trackerbutton == 1:  # button is a trigger, sets a state, which is cleared elsewhere (was_button_pressed)
-                        self.tracked_objects[trackerserial].buttons['system'] = True;
-                        self.tracked_objects[trackerserial].buttons['trigger'] = True;
+                        self.tracked_objects[trackerserial].buttons['system'] = True
+                        self.tracked_objects[trackerserial].buttons['trigger'] = True
 
-            sockreadable, _, _ = select.select([self.sock_trackerinfo], [], [], 0);
+            sockreadable, _, _ = select.select([self.sock_trackerinfo], [], [], 0)
 
         return 0
--- WIL_RemoteUDP.py
+++ WIL_RemoteUDP.py
@@ -12,40 +12,34 @@ import socket, struct
 class WILRemoteUDP:
 
     def __init__(self, destip, destport):
-        self.destip = destip;
-        self.destport = destport;
-        self.sock_trackerdata = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-        self.packetno_trackerdata = -1;
+        self.destip = destip
+        self.destport = destport
+        self.sock_trackerdata = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.packetno_trackerdata = -1
 
     def __del__(self):
-        self.sock_trackerdata.close();
+        self.sock_trackerdata.close()
 
     def send(self, tracker):
-        trackerpos    = tracker.get_raw_position();
-        trackeroriq   = tracker.get_raw_orientation_quat();
-        trackerbutton = tracker.was_button_pressed(["system", "trigger"]);
-        trackerserial = tracker.serial;
-        self.send_rawdata(trackerpos, trackeroriq, trackerbutton, trackerserial);
+        trackerpos    = tracker.get_raw_position()
+        trackeroriq   = tracker.get_raw_orientation_quat()
+        trackerbutton = tracker.was_button_pressed( ["system", "trigger"] )
+        trackerserial = tracker.serial
+        self.send_rawdata(trackerpos, trackeroriq, trackerbutton, trackerserial)
 
     def send_rawdata(self, trackerpos, trackeroriq, trackerbutton, trackerserial):
 
-        self.packetno_trackerdata = self.packetno_trackerdata + 1;
+        self.packetno_trackerdata = self.packetno_trackerdata + 1
         if self.packetno_trackerdata == 65536:
-            self.packetno_trackerdata = 0;
+            self.packetno_trackerdata = 0
 
-        packetno_trackerdata_hi = self.packetno_trackerdata >> 8;
-        packetno_trackerdata_lo = self.packetno_trackerdata & 0xFF;
+        packetno_trackerdata_hi = self.packetno_trackerdata >> 8
+        packetno_trackerdata_lo = self.packetno_trackerdata & 0xFF
 
-        data = bytearray(72);
-        data[ 0: 1] = [ packetno_trackerdata_hi, packetno_trackerdata_lo ];
-        data[ 2:14] = bytes(trackerserial, 'ascii');
-        data[14:22] = struct.pack("d", trackerpos[0]);
-        data[22:30] = struct.pack("d", trackerpos[1]);
-        data[30:38] = struct.pack("d", trackerpos[2]);
-        data[38:46] = struct.pack("d", trackeroriq[0]);
-        data[46:54] = struct.pack("d", trackeroriq[1]);
-        data[54:62] = struct.pack("d", trackeroriq[2]);
-        data[62:70] = struct.pack("d", trackeroriq[3]);
-        data[71]    = trackerbutton;
+        data = bytearray(72)
+        data[ 0: 1] = [ packetno_trackerdata_hi, packetno_trackerdata_lo ]
+        data[ 2:14] = bytes(trackerserial, 'ascii')
+        data[14:70] = struct.pack("ddddddd", trackerpos[0], trackerpos[1], trackerpos[2], trackeroriq[0], trackeroriq[1], trackeroriq[2], trackeroriq[3])
+        data[71]    = trackerbutton
 
-        self.sock_trackerdata.sendto(bytes(data), (self.destip, self.destport));
+        self.sock_trackerdata.sendto(bytes(data), (self.destip, self.destport))
